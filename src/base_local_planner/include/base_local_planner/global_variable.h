fd29a717cf76807105754b6a7e23c85c86236ae3
KangneoungLee <eehoa12381@gmail.com>
2023-01-30 21:07:38 -0600

    directory mod

---
+++ src/base_local_planner/include/base_local_planner/global_variable.h
@@ -0,0 +1,13 @@
+/*********************************************************************
+
+*********************************************************************/
+#ifndef GLOBAL_VARIABLE_H_
+#define GLOBAL_VARIABLE_H_
+
+#include <vector>
+
+extern unsigned char leg_mode_need;
+extern unsigned char leg_mode_current;
+
+
+#endif
